988e4b52be17419c756874d78a059faf3c11090f
Dani Sanchez <danysq87@gmail.com>
2021-11-07 11:31:45 +0100

    Created testUtils.js and FormCreate.test done

---
+++ src/components/forms/formCreate/FormCreate.test.js
@@ -0,0 +1,15 @@
+import renderWithProviders from "../../../utils/testUtils";
+import FormCreate from "./FormCreate";
+import { screen } from "@testing-library/react";
+
+describe("Given a component FormCreate", () => {
+  describe("When it's rendered'", () => {
+    test("Then it should render a form with a button create", () => {
+      renderWithProviders(<FormCreate />);
+
+      const elementForm = screen.getByRole("button", { name: /create/i });
+
+      expect(elementForm).toBeInTheDocument();
+    });
+  });
+});
