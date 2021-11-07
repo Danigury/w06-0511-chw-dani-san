88e16f646b563bfbfb36d27513894a03a75c596a
Dani Sanchez <danysq87@gmail.com>
2021-11-07 11:03:17 +0100

    bug fix removed from FormUpdate in App.js curly brackets

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import FormUpdate from "./components/forms/formUpdate/FormUpdate";
 // import FormCreate from "./components/forms/formCreate/FormCreate";
 
 function App() {
-  return <div className="App">{<FormUpdate />}</div>;
+  return (
+    <div className="App">
+      <FormUpdate />
+    </div>
+  );
 }
 
 export default App;
