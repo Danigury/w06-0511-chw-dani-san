4398c0f00121bd3198b026c712045a6d50d69a7a
dani-j-sanchez <78543811+Danigury@users.noreply.github.com>
2021-11-07 15:20:44 +0000

    Update App.js
    naming change of folder robotList

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import "./App.css";
 
-import RobotList from "./components/RobotList/robotList";
+import RobotList from "./components/robotList/robotList";
 import useRobots from "./hooks/useRobots";
 
 function App() {
